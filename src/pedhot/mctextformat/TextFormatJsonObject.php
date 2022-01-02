5113b856eb88bff13f7732fd71de609e5bc6757d
AgusPedhot <70968506+Pedhot-Dev@users.noreply.github.com>
2022-01-02 10:47:05 +0700

    Add files via upload

---
+++ src/pedhot/mctextformat/TextFormatJsonObject.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace pedhot\mctextformat;
+
+/**
+ * @internal
+ * @see TextFormat::toJSON()
+ */
+final class TextFormatJsonObject implements \JsonSerializable{
+	/** @var string|null */
+	public $text = null;
+	/** @var string|null */
+	public $color = null;
+	/** @var bool|null */
+	public $bold = null;
+	/** @var bool|null */
+	public $italic = null;
+	/** @var bool|null */
+	public $underlined = null;
+	/** @var bool|null */
+	public $strikethrough = null;
+	/** @var bool|null */
+	public $obfuscated = null;
+	/**
+	 * @var TextFormatJsonObject[]|null
+	 * @phpstan-var array<int, TextFormatJsonObject>|null
+	 */
+	public $extra = null;
+
+	public function jsonSerialize(){
+		$result = (array) $this;
+		foreach($result as $k => $v){
+			if($v === null){
+				unset($result[$k]);
+			}
+		}
+		return $result;
+	}
+}
